f3cf18e375d724cafccfeda9cec53c8b6b8b530c
Stephen Adams <StephenAdams.Minn@gmail.com>
2013-05-01 10:59:02 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clojure-intro-plugin "0.1.0-SNAPSHOT"
-  :description "A leinigan plugin for interacting with the clojure-intro-project"
+  :description "A leinigen plugin for interacting with the clojure-intro-project"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[intro 0.1]]
+  :dependencies [[intro "0.1"]]
   :repositories {"local" ~(str (.toURI (java.io.File. "jars")))})
